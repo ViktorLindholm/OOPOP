6608833c8f61e73de0565a3706e9a35cff0c85ea
ViktorLindholm <v_lindholm@hotmail.com>
2018-04-16 14:44:29 +0200

    Mouse

--- c++os/kernel.cpp
+++ c++os/kernel.cpp
@@ -3,7 +3,7 @@
 #include "gdt.h"
 #include "interrupts.h"
 #include "keyboard.h"
-
+#include "mouse.h"
 
 void printf(char* str)
 {
@@ -57,11 +57,12 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
-    printf("Upphill OS c++\n\n");
+    printf("Uphill OS\n");
 
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
     KeyboardDriver keyboard(&interrupts);
+    MouseDriver mouse(&interrupts);
     interrupts.Activate();
 
     while(1);
